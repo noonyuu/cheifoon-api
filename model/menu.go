ad2cf02338ffae42d53ebde477e4cd1b115997f7
shimizu <noon3m.op.sh@gmail.com>
2023-10-17 00:46:31 +0900

    [fix]undefinedを修正

--- model/menu.go
+++ model/menu.go
@@ -16,8 +16,8 @@ type Menu struct {
 }
 
 
-func CreateMenu(menu *model.Menu) error {
-    return db.Create(menu).Error
+func CreateMenu(menu *Menu) {
+	db.Create(menu)
 }
 
 func FindMenu(user_id *int, recipe_id *int) ([]Menu, error) {
